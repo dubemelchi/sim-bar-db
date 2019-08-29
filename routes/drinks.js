4f5f39c1090e160c931ed65ae199b5bbd6ddf719
dubemelchi <dubemelchi@gmail.com>
2019-08-29 19:57:55 -0400

    pull and insert routes

--- routes/drinks.js
+++ routes/drinks.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const db = require('../config/database');
 const drinks = require('../models/SimBar');
 
+// get drink list
 router.get('/', (req, res) => 
 drinks.findAll()
   .then(drinks => {
@@ -11,5 +12,23 @@ drinks.findAll()
   })
   .catch(err => console.log(err))
 );
+
+// add drink list
+router.get('/add', (req, res) => {
+  const data = {
+    drink_name: 'modelo',
+    drink_cost: 8.00
+  }
+
+  let { drink_name, drink_cost } = data;
+
+  // insert into table
+  drinks.create({
+    drink_name,
+    drink_cost
+  }) 
+  .then(drinks => res.redirect('/drinks'))
+  .catch(err => console.log(err));
+});
 
 module.exports = router;
